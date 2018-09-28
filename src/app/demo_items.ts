77510211088bf147229b76d35c2f3a7172436c40
TheCoderNextdoor <a.banerjee6762@gmail.com>
2018-09-28 23:39:55 +0530

    demo items now have a 'fav' property

--- src/app/demo_items.ts
+++ src/app/demo_items.ts
@@ -5,30 +5,35 @@ export const ITEMS: Item[] =
             id: 1,
             name: 'Laptop',
             value: 25,
-            info: 'A simple laptop, used for browsing the internet'
+            info: 'A simple laptop, used for browsing the internet',
+            fav: true
         },
         {
             id: 2,
             name: 'Torch',
             value: 2,
-            info: 'A torch, nothing special'
+            info: 'A torch, nothing special',
+            fav: false
         },
         {
             id: 3,
             name: 'RTX2080',
             value: 50,
-            info: 'Latest from Nvidia, with real time raytracing'
+            info: 'Latest from Nvidia, with real time raytracing',
+            fav: true
         },
         {
             id: 4,
             name: 'BluRay Disc',
             value: 12,
-            info: 'Has almost 25GB of storage'
+            info: 'Has almost 25GB of storage',
+            fav: true
         },
         {
             id: 5,
             name: 'Spectrometer',
             value: 20,
-            info: 'Wait, a what?'
+            info: 'Wait, a what?',
+            fav: true
         }
     ];
