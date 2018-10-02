724a7f16392634512df8e44affa6f0fa39a79c6b
TheCoderNextdoor <a.banerjee6762@gmail.com>
2018-10-02 19:31:21 +0530

    back function added

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Item } from '../item';
 import { ItemsComponent } from '../items/items.component';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-item-details',
@@ -11,9 +14,23 @@ export class ItemDetailsComponent implements OnInit {
 
   @Input() item: Item;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private itemService: ItemService,
+    private location: Location,
+  ) { }
 
   ngOnInit() {
+    this.getItem();
   }
 
+  getItem(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.itemService.getItem(id)
+    .subscribe(item => this.item = item);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
